d29517d61548f37c3dc76e9e5e795aefd43ec3d2
rbhadti94 <rbhadti@gmail.com>
2017-04-30 00:04:09 +0100

    Added dist folder

---
+++ src/statistics/base/BasicStats.java
@@ -0,0 +1,17 @@
+/**
+ * @author Ravindra Bhadti
+ * @V1.0
+ */
+
+package statistics.base;
+
+
+public class BasicStats {
+	
+	//mean.
+	//median.
+	//mode.
+	//variance
+	//standard deviation
+	//interquartile range.
+}
---
+++ src/statistics/dist/Distributions.java
@@ -0,0 +1,9 @@
+/**
+ * @author Ravindra Bhadti
+ * @V1.0
+ */
+package statistics.dist;
+
+public class Distributions {
+
+}
