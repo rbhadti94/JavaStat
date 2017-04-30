adfc1f378bc5d15afa1387c211105801b49b8510
rbhadti94 <rbhadti@gmail.com>
2017-04-30 15:59:22 +0100

    Added generic methods

--- src/statistics/base/BasicStats.java
+++ src/statistics/base/BasicStats.java
@@ -7,7 +7,7 @@ package statistics.base;
 
 import java.util.HashMap;
 
-public class BasicStats {
+public class BasicStats <T extends Number> {
 	
 	//---STAT BASIC METHOD---//
 	//mean.
@@ -35,7 +35,7 @@ public class BasicStats {
 	}
 	
 	public long mean(long a, long ...b){
-		return 0;
+		return ((mean(b)*b.length)+a)/(b.length+1);
 	}
 	
 	public long mean(long[] arr){
@@ -43,7 +43,7 @@ public class BasicStats {
 	}
 	
 	public float mean(float a, float ...b){
-		return 0;
+		return ((mean(b)*b.length)+a)/(b.length+1);
 	}
 	
 	public float mean(float[] arr){
@@ -51,26 +51,31 @@ public class BasicStats {
 	}
 	
 	public double mean(double a, double ...b){
-		return 0;
+		return ((mean(b)*b.length)+a)/(b.length+1);
 	}
 	
 	public double mean(double[] arr){
 		return 0;
 	}
 	
-	public < T extends Number > T mean(T a, T ...b){
-		return a;
+	public double mean(T a, T ...b){
+		return ((mean(b)*b.length)+a.doubleValue())/(b.length+1);
 	}
 	
-	public < T extends Number > T mean( T[] arr){
-		return (T)(new Object());
+	public double mean( T[] arr){
+		Double arrSum = 0.0;
+		
+		for(int i = 0; i < arr.length; i++){
+			arrSum += arr[i].doubleValue();
+		}
+		
+		return arrSum/arr.length;
+		
 	}
 	
-	public < T extends Number > T mean( HashMap<T,T> hmap){
+	public T mean( HashMap<T,T> hmap){
 		return null;
 	}
-	
-	//
 	
 	public double standardDev(int a, int ...b){
 		return 0;
@@ -85,6 +90,14 @@ public class BasicStats {
 	}
 	
 	public double standardDev(double a, double ...b){
+		return 0;
+	}
+	
+	public double standardDev(T[] arr){
+		return 0;
+	}
+	
+	public double standardDev(T a, T ...b){
 		return 0;
 	}
 }
