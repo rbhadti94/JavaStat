b3bba1504d58fc5572bbba3ada3d2d45d69622a9
rbhadti94 <rbhadti@gmail.com>
2017-05-02 20:09:30 +0100

    Finished all median methods

--- src/statistics/base/BasicStats.java
+++ src/statistics/base/BasicStats.java
@@ -267,39 +267,93 @@ public strictfp class BasicStats  {
 	/* -------- MEDIAN -------- */
 	public final static double median(int[] arr){
 		Arrays.sort(arr);
-		return 0;
+		
+		double tmpArray[] = new double[arr.length];
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length; i++){
+			tmpArray[i] = arr[i];
+		}
+		
+		return median(tmpArray);
 	}
 	
 	public final static double median(int a, int ...b){
-		
-		return 0;
+		int tmpArray[] = new int[b.length+1];
+		tmpArray[0] = a;
+		for(int i = 1; i <= b.length; i++){
+			tmpArray[i] = b[i-1];
+		}
+		return median(tmpArray);
 	}
 	
 	public final static double median(long[] arr){
 		Arrays.sort(arr);
-		return 0;
+		double tmpArray[] = new double[arr.length];
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length; i++){
+			tmpArray[i] = arr[i];
+		}
+		
+		return median(tmpArray);
 	}
 	
 	public final static double median(long a, long ...b){
-		return 0;
+		long tmpArray[] = new long[b.length+1];
+		tmpArray[0] = a;
+		for(int i = 1; i <= b.length; i++){
+			tmpArray[i] = b[i-1];
+		}
+		return median(tmpArray);
 	}
 	
 	public final static double median(float[] arr){
+		double tmpArray[] = new double[arr.length];
 		Arrays.sort(arr);
-		return 0;
+		for(int i = 0; i < arr.length; i++){
+			tmpArray[i] = arr[i];
+		}
+		
+		return median(tmpArray);
 	}
 	
 	public final static double median(float a, float ...b){
-		return 0;
+		float tmpArray[] = new float[b.length+1];
+		tmpArray[0] = a;
+		for(int i = 1; i <= b.length; i++){
+			tmpArray[i] = b[i-1];
+		}
+		return median(tmpArray);
 	}
 	
 	public final static double median(double[] arr){
+			
+		//Sort the array.
 		Arrays.sort(arr);
-		return 0;
+		
+		int inputArraySize = arr.length;
+		double medVal = 0;
+		
+		//Odd size array.
+		if(arr.length%2==1){
+			medVal = arr[(int)Math.floor(inputArraySize/2)];
+		}
+		//Even size array.
+		else{
+			medVal = (double)(arr[(inputArraySize/2)-1]+arr[(inputArraySize/2)])/2;
+		}
+		
+		return medVal;
 	}
 	
 	public final static double median(double a, double ...b){
-		return 0;
+		double tmpArray[] = new double[b.length+1];
+		tmpArray[0] = a;
+		for(int i = 1; i <= b.length; i++){
+			tmpArray[i] = b[i-1];
+		}
+		return median(tmpArray);
 	}
 	
 	
