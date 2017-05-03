c3882d8b6906f0a27672267eb642f4857f55c7f5
rbhadti94 <rbhadti@gmail.com>
2017-05-03 22:39:31 +0100

    Added step sizes for continuous distributions

--- src/statistics/dist/Distributions.java
+++ src/statistics/dist/Distributions.java
@@ -67,25 +67,63 @@ public strictfp class Distributions {
 	}
 	
 	/**
+	 * The cumulative probability for the exponential distribution.
+	 * Returns F(X) = 1-e^(-lambda*x) for X = x.
+	 * 
+	 * @param lambda - The decay constant.
+	 * @param x - The parameter to test for.
+	 * @return double - P(X<x;lambda)
+	 */
+	public static final double cumulProbExponential(double lambda, double x){
+		return (1-Math.pow(Math.E, -lambda*x));
+	}
+	
+	/**
 	 * This method returns the values P(X=x;lambda) for
 	 * x in range [0,xMax-1]. It returns the distribution as 
 	 * an array. 
 	 * 
 	 * @param lambda - Decay constant.
 	 * @param xMax - The maximum x value to go up to.
-	 * @return double[] - The return distribution.
+	 * @return double[] - The output distribution.
 	 */
-	public static final double[] distExponential(double lambda, int xMax){
+	public static final double[] pdf_Exponential(double lambda, int xMax, double step){
 		assert(xMax <= Integer.MAX_VALUE);
-		double exponentialDist[] = new double[xMax];
-		for(int x = 0; x < xMax; x++){
+		assert(step < xMax); 
+		double exponentialDist[] = new double[(int)Math.floor((double)xMax/step)];
+		for(int x = 0; x < Math.floor((double)xMax/step); x+=step){
 			exponentialDist[x] = probExponential(lambda, x);
 		}
 		return exponentialDist;
 	}
 	
+	/**
+	 * 
+	 * @param lambda
+	 * @param k
+	 * @return double - Pr(X=k) for a poisson dist. with mean lambda.
+	 */
 	public static final double probPoisson(double lambda, int k){
 		return Math.pow(lambda, k)*Math.pow(Math.E, -lambda)/factorial(k);
+	}
+	
+	/**
+	 * 
+	 * @param lambda - The parameter.
+	 * @param step - The array step size.
+	 * @return - double[] The output Poisson distribution.
+	 */
+	public static final double[] distPoission(double lambda, int N, double step){
+		
+		assert(step < N);
+		
+		double poissonDist[] = new double[(int)Math.floor(N/step)];
+		
+		for(int i = 0; i < (int)Math.floor(N/step) ; i+=step){
+			poissonDist[i] = probPoisson(lambda, i);
+		}
+		
+		return poissonDist;
 	}
 	
 	/**
@@ -94,23 +132,27 @@ public strictfp class Distributions {
 	 * 
 	 * @param mu - The Mean.
 	 * @param sigma - The Standard Deviation.
+	 * @param step - Step for the array.
 	 * @return double[] - The normal distribution N(mu, sigma^2);
 	 * 
 	 */
-	public static final double[] distNormal(double mu, double sigma, int numElem){
+	public static final double[] distNormal(double mu, double sigma, int numElem, double step){
 		
-		double normalDist[] = new double[2*numElem+1];
+		int totalNumElem = 2*numElem + 1;
+		
+		assert(step < totalNumElem);
+		
+		double normalDist[] = new double[(int)Math.floor(totalNumElem/step)];
 		double variance = sigma*sigma;
 		double coeff = 1/(Math.sqrt(2*variance*Math.PI));
 		double expRes = 0;
 		
-		for(int i = -numElem; i < numElem ; i++){
+		for(int i = -numElem; i < -numElem+(Math.floor(totalNumElem/step)) ; i+=step){
 			expRes = Math.pow(Math.E, -((i-mu)*(i-mu))/(2*variance));
 			normalDist[i] = coeff*expRes;
 		}
 		
 		return normalDist;
-		
 	}
 	
 	/**
@@ -126,7 +168,6 @@ public strictfp class Distributions {
 		
 		return 0.5*(1+erf(erfCoeff));
 	}
-	
 		
 	/*---------- SIDE METHODS -----------*/
 	
