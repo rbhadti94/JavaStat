ffeff6f0ff9f675ba12b230a2a9948e0f5c30fff
rbhadti94 <rbhadti@gmail.com>
2017-05-07 16:45:31 +0100

    Added generic type methods for LSR

--- src/statistics/regression/Regression.java
+++ src/statistics/regression/Regression.java
@@ -313,6 +313,29 @@ public strictfp class Regression {
 		return leastSquareRegression(XTemp, YTemp);
 	}
 	
+	/**
+	 * 
+	 * @param X -
+	 * @param Y - 
+	 * @return Double[] - 
+	 */
+	public final static <T extends Number> Double[] leastSquareRegression(T X[], T Y[]){
+		
+		double XTemp[] = new double[X.length];
+		double YTemp[] = new double[Y.length];
+		
+		for(int i = 0; i < X.length; i++){
+			XTemp[i] = X[i].doubleValue();
+			YTemp[i] = Y[i].doubleValue();
+		}
+		
+		double res[] = leastSquareRegression(XTemp, YTemp);
+		
+		Double returnVal[] = {Double.valueOf(res[0]),Double.valueOf(res[1])};
+		
+		return returnVal;
+	}
+	
 	
 	
 }
